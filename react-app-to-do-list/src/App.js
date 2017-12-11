3a304a75be3c38168dd7d196e5982c0f1913514c
Michal Turek <michael.turek@rizk.com>
2017-12-11 11:13:52 +0100

    Added material ui

--- react-app-to-do-list/src/App.js
+++ react-app-to-do-list/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Copyright by Italik</h1>
-        </header>
-        <p className="App-intro">
-          This is React App
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <RaisedButton label="Default" />
+      </MuiThemeProvider>
     );
   }
 }
