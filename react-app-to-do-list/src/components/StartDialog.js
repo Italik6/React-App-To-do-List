f28b7a9c8503e428fca2fcb05086ba14fe14c8a7
Michal Turek <michael.turek@rizk.com>
2017-12-12 09:56:17 +0100

    Add Button

--- react-app-to-do-list/src/components/StartDialog.js
+++ react-app-to-do-list/src/components/StartDialog.js
@@ -6,33 +6,41 @@ import TextField from 'material-ui/TextField';
 
 export default class StartDialog extends Component {
     constructor(props) {
-        super(props)
-        this.state = { errorText: '', value: props.value, open:true }
+        super(props);
+        this.state = { errorText: '', value: props.value, open:false };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
       }
   
       handleSubmit = (event) => {
         let password = this.refs.myPasswordValue.input.value;
-
-          if(password === "12345"){
+    // Check default password
+          if(password === "123"){
                 this.setState({ errorText: '' })
                 this.setState({open: false});
-          } else if (password !== "12345" && password !== "") {
+          } else if (password !== "123" && password !== "") {
                 this.setState({ errorText: "Password is incorrect" })
           } else if (password === ""){
                 this.setState({ errorText: "Password is required" })
           }
       };
 
+      handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+         this.handleSubmit(event);
+        }
+      }
+
   render() {
     const actions = [
-        <FlatButton label="Submit" primary={true} onClick={this.handleSubmit} />,
+        <FlatButton label="Submit" primary={true} onClick={this.handleSubmit}  />,
     ];
 
     return (
         <Dialog title="Welcome to the React App!" actions={actions} modal={true} open={this.state.open} >
             <p className="StartDialog-subheader">To use the application it is necessary to enter the password.<br/>
-            (default password: 12345)</p>
-            <TextField errorText={this.state.errorText} hintText="Password Field" floatingLabelText="Password" type="password" ref="myPasswordValue" />
+            (default password: 123)</p>
+            <TextField errorText={this.state.errorText} hintText="Password Field" floatingLabelText="Password" type="password" ref="myPasswordValue" onKeyPress={this.handleKeyPress}/>
         </Dialog>
     );
   }
--- react-app-to-do-list/src/containers/Home.js
+++ react-app-to-do-list/src/containers/Home.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import StartDialog from '../components/StartDialog'
-
+import './Home.css';
+import StartDialog from '../components/StartDialog';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 export default class Home extends Component {
 
+  handleClick = (event) => {
+   
+  }
+
   render() {
     return (
-      <StartDialog />
+      <div>
+        <StartDialog />
+        <FloatingActionButton onClick={this.handleClick} className="Home-AddButton">
+          <ContentAdd />
+        </FloatingActionButton>
+      </div>
     );
   }
 }
